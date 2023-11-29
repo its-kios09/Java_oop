1dd09842104cfe6888845637e796b6261f0b5c88
blackcode-creator <legacyitsolution@gmail.com>
2023-11-29 17:20:23 +0300

    Object oriented programming with java

---
+++ GetStarted.java
@@ -0,0 +1,9 @@
+public class GetStarted {
+    public static void main(String[] args){
+//        Declaring variables
+        String string = "Java programming has started";
+
+//        Printing the sring
+        System.out.println(string);
+    }
+}
---
+++ JavaTokens.java
@@ -0,0 +1,2 @@
+public class JavaTokens {
+}
