920bcfda8ea040fd72c823d12e3840f6f47c66b9
blackcode-creator <legacyitsolution@gmail.com>
2023-11-30 12:44:38 +0300

    arithemtic operators

---
+++ Operators.java
@@ -0,0 +1,25 @@
+public class Operators {
+//    Operators are used to perform operation on variables and values
+    public static void main(String [] args ){
+//        declaring the variables
+        int a = 2;
+        int b = 6;
+        int c = 0;
+
+//   Arithmetic operator
+        c = a + b;
+        System.out.println("Classifying of Operators:-");
+        System.out.println("Arithmetic Operation (Addition):- "+ c);
+
+        c = a * b;
+        System.out.println("Arithmetic Operation (Multiplication):- "+ c);
+
+        c = a - b;
+        System.out.println("Arithmetic Operation (Subtraction):- "+ c);
+
+        c = b / a;
+        System.out.println("Arithmetic Operation (Division):- "+ c);
+
+
+    }
+}
