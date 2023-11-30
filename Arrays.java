30bccb023acef75a74e30eed01143f97bae84692
blackcode-creator <legacyitsolution@gmail.com>
2023-11-30 10:15:17 +0300

    added

---
+++ Arrays.java
@@ -0,0 +1,20 @@
+public class Arrays {
+    public static void main(String[] args){
+//        An array is a container object that holds a fixed number of values of a single type
+//         Declaration and allocating  of an Array
+        int[] array = new int[10];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 4;
+        array[3] = 10;
+        array[4] = 23;
+
+//        accessing the values using the index
+        System.out.println("index 0 the value is "+ array[0]);
+        System.out.println("index 1 the value is "+ array[1]);
+        System.out.println("index 2 the value is "+ array[2]);
+        System.out.println("index 3 the value is "+ array[3]);
+        System.out.println("index 4 the value is "+ array[4]);
+
+    }
+}
--- JavaTokens.java
+++ JavaTokens.java
@@ -1,2 +1,24 @@
 public class JavaTokens {
+//    token is the smallest element in a java program
+//    which they can be as classified:- Keywords, Identifiers, constants, Special symbols and operators
+    public static void main(String[] args){
+//        Type casting is when you assign a value of one primitive data type to another type.
+//        Implicit data type conversion(wide type casting)
+        int a = 100;
+//        Am using inline variable. - Syntax:- (keyword) variable_name
+        System.out.println("Implicit data type conversion(wide type casting)");
+        System.out.println("int representation is: "+ a);
+        System.out.println("long representation is: "+ (long) a);
+        System.out.println("float representation is:"+ (float) a + "\n");
+
+//        Explicit data type conversion(Narrow Type Casing)
+        double x = 10.34;
+        System.out.println("Explicit data type conversion(wide type casting)");
+        System.out.println("double representation is: "+ x);
+        System.out.println("float representation is: "+ (float) x);
+        System.out.println("int representation is:"+ (int) x);
+
+
+
+    }
 }
