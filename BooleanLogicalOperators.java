d595df06f3ca0fe5eb8eb3ac314df7a57afd58e8
blackcode-creator <legacyitsolution@gmail.com>
2023-12-02 16:46:52 +0300

    Boolean Logical Operator

---
+++ BooleanLogicalOperators.java
@@ -0,0 +1,21 @@
+public class BooleanLogicalOperators {
+//    Boolean Logical Operators are such as | , &
+    public static void main(String[] args){
+        boolean a = true;
+        boolean b = false;
+        boolean c = a | b;
+        boolean d = a & b;
+        boolean e = a ^ b;
+        boolean f = !a;
+
+        System.out.println("a:- " + a);
+        System.out.println("b:- " + b);
+        System.out.println("a | b:- " + c);
+        System.out.println("a & b:- " + d);
+        System.out.println("a ^ b:-" + e);
+        System.out.println("!a:- " + f);
+
+
+
+    }
+}
