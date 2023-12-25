0760f5300d961d1d1fd79184246f6c16a7f483c3
blackcode-creator <legacyitsolution@gmail.com>
2023-12-25 15:05:56 +0300

    added regex

--- RegularExpression.java
+++ RegularExpression.java
@@ -1,5 +1,15 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegularExpression {
     public static void main(String[] args){
-        
+//        (.) matches any character (/w) - any a-z or 0-9
+        String regex = "\\w";
+        String text = "3";
+        Pattern pt = Pattern.compile(regex);
+        Matcher mt = pt.matcher(text);
+
+        boolean results_of_expression = mt.matches();
+        System.out.println("Result:- " + results_of_expression);
     }
 }
